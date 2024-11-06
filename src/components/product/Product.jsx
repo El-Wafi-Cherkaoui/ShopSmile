cf3541b3694f9a7f9d17500737424bebe420dafb
Cherkaoui El Wafi <elwafi.cherkaoui@gmail.com>
2024-11-06 14:00:45 +0100

    Fix: bug of Add_to_cart button visibility logic
    - Ensures that if the customer has pinned all the quantity available
    of the product, the Add_to_cart button do not render.

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import '../../styles/App.css'
 import { Link, Outlet, useLoaderData } from 'react-router-dom'
 import Stock from './Stock'
 import Add_to_cart from './Add_to_cart'
 import QuantityController from './QuantityController'
+import { AppContext } from '../../routes/App'
 
 export const ProductContext = React.createContext()
 
@@ -11,6 +12,15 @@ export default function Product({product}) {
   if(product.quantity == 0) return 
   const [q_controller_visible, setQCV] = useState(false)
   const [add_to_cart_btn, setATC] = useState(true)
+  const {myCart} = useContext(AppContext)
+  
+  function product_is_available() {
+    let my_order = myCart.filter((order)=>order.ordered_product.id == product.id)[0]
+    if (!my_order || my_order.ordered_quantity < product.quantity) return true
+    else{
+      return false
+    }
+  }
   return(
 
     <div className='product' key={product.id}>
@@ -24,7 +34,7 @@ export default function Product({product}) {
         <ProductContext.Provider value={{product, q_controller_visible, setQCV, add_to_cart_btn, setATC}} >
           <Stock />
           <QuantityController />
-          <Add_to_cart/>
+          {product_is_available() ? <Add_to_cart/> : ''}
         </ProductContext.Provider>
       </div>
 
