10bc138917a8cb793ad894614b6d1267cab7ac08
Cherkaoui El Wafi <elwafi.Cherkaoui@gmail.com>
2024-11-11 21:59:13 +0100

    Improve UI: Error page design improvement

--- src/components/Error_page.jsx
+++ src/components/Error_page.jsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function Error_page(){
 
     return(
         <div className="error_box">
-            <h1>Error title</h1>
-            Page You trying to Access is not found .
+            <h1>Error !</h1>
+            <p>
+                Page You trying to Access is not found . <br />
+                <Link to={'/'}>Go back</Link>
+            </p>
         </div>
     )
 }
